232d7045f186ed0b8c2a85642ad9006d95f48330
Subject-2407 <stevanputra.bintang@gmail.com>
2025-11-27 21:30:43 +0700

    chore: add TODO comments for future improvements

--- src/main/java/com/scapes/impl/WindowsSystemHandler.java
+++ src/main/java/com/scapes/impl/WindowsSystemHandler.java
@@ -6,14 +6,14 @@ import java.io.File;
 public class WindowsSystemHandler implements SystemHandler {
     @Override
     public File downloadImage(String url, String filename) {
-        // TODO: Tim Implementasi buat logika download file di sini
+        // TODO: ini logika download file di sini (khusus backend)
         // Gunakan java.nio.file.Files.copy() dari URL stream
         return new File("path/to/" + filename); // Placeholder
     }
 
     @Override
     public boolean setWallpaper(File imageFile) {
-        // TODO: Masukkan kode JNA User32 yang saya beri sebelumnya di sini
+        // TODO: ini logika set wallpaper di Windows menggunakan JNA (khusus backend)
         System.out.println("Setting wallpaper to: " + imageFile.getAbsolutePath());
         return true; 
     }
