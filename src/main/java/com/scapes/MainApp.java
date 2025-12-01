6b2887b0bd1ea64fd7e0cef6b01f982406366d59
Subject-2407 <stevanputra.bintang@gmail.com>
2025-12-01 17:33:17 +0700

    feat(frontend-main): add scapes icon

--- src/main/java/com/scapes/MainApp.java
+++ src/main/java/com/scapes/MainApp.java
@@ -9,10 +9,12 @@ import com.scapes.impl.WindowsSystemHandler;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
     private static final String APP_TITLE = "4scapes";
+    private static final String APP_ICON = "/com/scapes/icon/scapes.png";
     private static final String MAIN_VIEW_FXML = "/com/scapes/view/main_view.fxml";
     private static final String APP_STYLE = "/com/scapes/view/style.css";
 
@@ -21,6 +23,10 @@ public class MainApp extends Application {
         FXMLLoader loader = new FXMLLoader(getClass().getResource(MAIN_VIEW_FXML));
         String css = this.getClass().getResource(APP_STYLE).toExternalForm();
         Scene scene = new Scene(loader.load());
+        Image appIcon = new Image(getClass().getResourceAsStream(APP_ICON));
+        if (appIcon != null) {
+            stage.getIcons().add(appIcon);
+        }
 
         scene.getStylesheets().add(css);
         stage.initStyle(javafx.stage.StageStyle.UNDECORATED);
